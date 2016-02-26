256323cbdc7c19cb302fb6186ddc176d6423156a
amandeep1509 <asing609@my.centennialcollege.ca>
2016-02-26 18:27:04 -0400

    Player and rewards added

--- Assets/_Scripts/HeroController.cs
+++ Assets/_Scripts/HeroController.cs
@@ -1,6 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// VELOCITY RANGE UTILITY Class +++++++++++++++++++++++
+[System.Serializable]
+public class VelocityRange
+{
+    // PUBLIC INSTANCE VARIABLES ++++
+    public float minimum;
+    public float maximum;
+
+    // CONSTRUCTOR ++++++++++++++++++++++++++++++++++++
+    public VelocityRange(float minimum, float maximum)
+    {
+        this.minimum = minimum;
+        this.maximum = maximum;
+    }
+}
 
 public class HeroController : MonoBehaviour
 {
@@ -10,53 +25,145 @@ public class HeroController : MonoBehaviour
 	private float _jump;
 	private bool _facingRight;
 	private Transform _transform;
+    private Rigidbody2D _rigidBody2D;
+    private bool _isGrounded;
 
+    // PUBLIC INSTANCE VARIABLES
+    public VelocityRange velocityRange;
+    public float moveForce;
+    public float jumpForce;
+    public Transform groundCheck;
+    public Transform camera;
+    public GameController gameController;
 
-	// Use this for initialization
-	void Start () {
-		this._transform = gameObject.GetComponent<Transform> ();
+    // Use this for initialization
+    void Start () {
+        // Initialize Public Instance Variables
+        this.velocityRange = new VelocityRange(300f, 30000f);
+
+        this._transform = gameObject.GetComponent<Transform> ();
 		this._animator = gameObject.GetComponent<Animator> ();
-		this._move = 0f;
+        this._rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+        this._move = 0f;
 		this._jump = 0f;
 		this._facingRight = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		this._move = Input.GetAxis ("Horizontal");
-		this._jump = Input.GetAxis ("Vertical");
-		Debug.Log (this._jump);
-		if (this._move != 0) {
-			if (this._move > 0) {
-				this._facingRight = true;
-				this._flip ();
-			}
-			if (this._move < 0) {
-				this._facingRight = false;
-				this._flip ();
-			}
 
-			// call the walk clip
-			this._animator.SetInteger ("AnimState", 1);
-		} else {
-			
-			// set default animation state to "idle"
-			this._animator.SetInteger ("AnimState", 0);
-		}
-        if (this._jump > 0)
+        // place the hero in the starting position
+        this._spawn();
+    }
+
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this._isGrounded = Physics2D.Linecast(
+            this._transform.position,
+            this.groundCheck.position,
+            1 << LayerMask.NameToLayer("Ground"));
+        Debug.DrawLine(this._transform.position, this.groundCheck.position);
+        Vector3 currentPosition = new Vector3(this.transform.position.x,this.transform.position.y,-10);
+        this.camera.position = currentPosition;
+
+
+        float forceX = 0f;
+        float forceY = 0f;
+
+        // get absolute value of velocity for our gameObject
+        float absVelX = Mathf.Abs(this._rigidBody2D.velocity.x);
+        float absVelY = Mathf.Abs(this._rigidBody2D.velocity.y);
+
+        // Ensure the player is grounded before any movement checks
+        if (this._isGrounded)
         {
+           
+            // gets a number between -1 to 1 for both Horizontal and Vertical Axes
+            this._move = Input.GetAxis("Horizontal");
+            this._jump = Input.GetAxis("Vertical");
+
+            if (this._move != 0)
+            {
+                if (this._move > 0)
+                {
+                    // movement force
+                    if (absVelX < this.velocityRange.maximum)
+                    {
+                        forceX = this.moveForce;
+                    }
+                    this._facingRight = true;
+                    this._flip();
+                }
+                if (this._move < 0)
+                {
+                    // movement force
+                    if (absVelX < this.velocityRange.maximum)
+                    {
+                        forceX = -this.moveForce;
+                    }
+                    this._facingRight = false;
+                    this._flip();
+                }
+
+                // call the walk clip
+                this._animator.SetInteger("AnimState", 1);
+            }
+            else {
+
+                // set default animation state to "idle"
+                this._animator.SetInteger("AnimState", 0);
+            }
+
+            
+            if (this._jump > 0)
+            {
+                // jump force
+                if (absVelY < this.velocityRange.maximum)
+                {
+                    forceY = this.jumpForce;
+                }
+
+            }
+            
+        }
+        else {
             // call the "jump" clip
             this._animator.SetInteger("AnimState", 1);
         }
 
+      //  Debug.Log(forceX);
+        // Apply the forces to the player
+        this._rigidBody2D.AddForce(new Vector2(forceX, forceY));
     }
 
-	// PRIVATE METHODS
-	private void _flip() {
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Death"))
+        {
+            this._spawn();
+            this.gameController.LivesValue--;
+        }
+
+
+    if(other.gameObject.CompareTag("Gift")) {
+		//	this._coinSound.Play ();
+
+            Destroy(other.gameObject);
+			this.gameController.ScoreValue += 10;
+		}
+}
+
+
+// PRIVATE METHODS
+private void _flip() {
 		if (this._facingRight) {
 			this._transform.localScale = new Vector2 (1, 1);
 		} else {
 			this._transform.localScale = new Vector2 (-1, 1);
 		}
 	}
+
+    private void _spawn()
+    {
+        this._transform.position = new Vector3(-105f, 65f, 0);
+    }
 }
+
